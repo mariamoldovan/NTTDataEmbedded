0522c86599fdbd362983634422e511bb433385f6
Sergiu71 <41329987+Sergiu71@users.noreply.github.com>
2018-07-24 15:52:07 +0300

    Buzzer

---
+++ Buzzerclass.cpp
@@ -0,0 +1,63 @@
+#include "Buzzerclass.h"
+#include <toneAC.h>
+
+Buzzer* Buzzer::instance = 0;
+ 
+Buzzer* Buzzer :: getinstance()
+{
+    if (instance == 0)
+    {
+        instance = new Buzzer();
+    }
+
+    return instance;
+}
+
+Buzzer :: Buzzer(){
+  
+  _frequency = 100;
+  _duration = 10;
+  _volume = 3 ;
+}
+
+/*Buzzer :: Buzzer(unsigned int frequency ,unsigned int duration , unsigned int volume){
+  Serial.print("Obiectul de tip buzzer a fost instantiat !!!");
+  _frequency = frequency;
+  _duration = duration;
+  _volume = volume ;
+}*/
+
+Buzzer :: ~Buzzer(){
+ _frequency=0;
+ _duration=0;
+ _volume=0;
+}
+
+void Buzzer :: RingBuzzer()
+{
+  
+  toneAC(_frequency,_volume,_duration);
+}
+
+void Buzzer :: RingBuzzer(unsigned int frequency ,unsigned int duration , unsigned int volume)
+{
+  toneAC(frequency,duration,volume);
+}
+
+void Buzzer :: SilentBuzzer(){
+  noToneAC();
+  toneAC(0);
+}
+
+void Buzzer :: set_freq(int frequency){
+  _frequency=frequency;
+}
+
+void Buzzer :: set_volume(int volume){
+  _volume = volume;
+}
+
+void Buzzer :: set_duration(int duration){
+  _duration= duration ;
+}
+
---
+++ Buzzerclass.h
@@ -0,0 +1,45 @@
+#ifndef buzzer
+#define buzzer
+
+#if (ARDUINO >=100)
+  #include "Arduino.h"
+#else 
+  #include "WProgram.h"
+  #endif
+
+  // CONNECTION:
+ //   Pins  9 & 10 - ATmega328, ATmega128, ATmega640, ATmega8, Uno, Leonardo, etc.
+ //   Pins 11 & 12 - ATmega2560/2561, ATmega1280/1281, Mega
+ //   Pins 12 & 13 - ATmega1284P, ATmega644
+ //   Pins 14 & 15 - Teensy 2.0
+ //   Pins 25 & 26 - Teensy++ 2.0
+
+class Buzzer {
+  
+  private://variabile private 
+  unsigned int _frequency;
+  unsigned int _duration;
+  unsigned int _volume ;
+  static Buzzer* instance;
+  Buzzer();
+  //Buzzer (unsigned int frequency ,unsigned int duration , unsigned int volume );//constructor
+ 
+  
+  public :
+ ~Buzzer();//destructor
+ static Buzzer* getinstance();
+ 
+ //Metode 
+  void RingBuzzer();//functie pentru a genera sunet de la buzzer fara parametri
+  void RingBuzzer(unsigned int frequency ,unsigned int duration , unsigned int volume);//functie pentru a genera sunet de la buzzer cu parametri
+  void SilentBuzzer();//functie pentru a opri sunetul generat de buzzer 
+  
+  void set_freq(int frequency);//setam doar frecventa
+  void set_duration(int duration);//setam doar duratia
+  void set_volume(int volume);//setam doar volumul 
+  
+};
+
+#endif 
+
+
