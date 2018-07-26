e29e59ad8c565ab4e7b3ea69803630e34082c2f8
MMoollddoovvaann <37744053+MMoollddoovvaann@users.noreply.github.com>
2018-07-26 15:59:04 +0300

    Versiune finala Testata

--- bluetooth/Bluetooth.h
+++ bluetooth/Bluetooth.h
@@ -19,7 +19,7 @@ class Bluetooth
     Bluetooth(uint16_t piniconfig[], uint16_t baund);
     void construireFrame(uint8_t data[], uint8_t tramnsmite[9]);
     uint16_t paritate(uint8_t dist);
-    int16_t decodificareFrame(String mesajVolum); 
+    uint8_t decodificareFrame(uint8_t mesajVolum); 
 };
 
 
