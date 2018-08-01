a8fa5771c86d9b3a7f71f366995be3459d3e024b
MMoollddoovvaann <37744053+MMoollddoovvaann@users.noreply.github.com>
2018-08-01 09:49:19 +0300

    Add files via upload

---
+++ bluetooth_4/bluetooth.ino
@@ -0,0 +1,21 @@
+//Test Bluetooth
+#include "Bluetooth.h"
+
+
+int pini[2] = {5, 6};
+Bluetooth *blue = Bluetooth::getInstance(pini, 9600);
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("de la Rasp");
+  char dateDist[5] = {88, 199, 77, 21, 98};
+  (*blue).trimiteDateRaspberry(dateDist, 0);
+  //uint16_t dist = (*blue).primesteDateRaspberry();
+  //Serial.println("\t"+dist);
+  //(*blue).trimiteDateRaspberry(dateDist, 1);
+  delay(1000);
+}
