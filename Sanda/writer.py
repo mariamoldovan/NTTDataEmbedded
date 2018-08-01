b3d5c1e95fc8792d1d606e96d2f3d69b215c482c
catapinzar <catalin.pinzar@gmail.com>
2018-08-01 09:42:50 +0100

    GUI + Raspberry

--- Sanda/writer.py
+++ Sanda/writer.py
@@ -1,7 +1,7 @@
 import os
 import sys
 while True:
-	path = "./myfifo"
+	path = "/home/pi/Senzor/FIFO/myfifo"
 	fifo = open(path, "r")
 	line = fifo.read()
 	fifo.close()
@@ -9,13 +9,13 @@ while True:
 	#print(len(line))
 	#print( [ bin(ord(ch))[2:].zfill(8) for ch in line ] )
 	if line=="Send data!\0":
-		print ("Data request was send!")
+		#print ("Data request was send!")
 		fifo = open(path, "w")
-		fifo.write("Distante!\0")
+		fifo.write("140 200 20 10 32\0")
 		fifo.close()
 		#print("Distante trimise")
 		#fifo.flush()
-	else:
-		print ("Data request was not send!")
+	#else:
+		#print ("Data request was not send!")
 #fifo.close()
 #os.unlink(path)
--- Sebi/decodifComentatV4.1Sebi/decodificareDate.py
+++ Sebi/decodifComentatV4.1Sebi/decodificareDate.py
@@ -5,7 +5,7 @@ def decodificareDate():
     fisierCitireDate = open("date.txt", "r")
     frameBinar = []
 
-    locatieFifo = "./myfifo"
+    locatieFifo = "/home/pi/Downloads/Senzor-1-AUG/FIFO/myfifo"
     fifoGUI = open(locatieFifo, "r")
     linieDateFifo = fifoGUI.read()
     fifoGUI.close()
@@ -14,24 +14,24 @@ def decodificareDate():
         for frame in listaFrameuri:
             if len(frame) > 7: #eliminare date incomplete
                 lungimiSenzoriInDecimal = []
-                print("IN")
+                #print("IN")
                 frameBinar = [ bin(ord(byte))[2:].zfill(8) for byte in frame ] #convertire frame in baza 2
-                print(frameBinar) #afisez tot frameul
+                #print(frameBinar) #afisez tot frameul
                 detectieErori = frameBinar[6] #pregatesc parcurgerea unui frame bit cu bit
                 for pozitie in range (1,6):
-                    print(frameBinar[pozitie]) #afisez lungimile
-                    print(frameBinar[6]) #afisez octetul de corectie
-                    print(detectieErori[pozitie+1]) #parcurg octetul de corectie bit cu bit
-                    print("S["+str(pozitie)+"]"+" are "+str(frameBinar[pozitie].count('1'))+" biti de 1")
+                    #print(frameBinar[pozitie]) #afisez lungimile
+                    #print(frameBinar[6]) #afisez octetul de corectie
+                    #print(detectieErori[pozitie+1]) #parcurg octetul de corectie bit cu bit
+                    #print("S["+str(pozitie)+"]"+" are "+str(frameBinar[pozitie].count('1'))+" biti de 1")
 
                     if (frameBinar[pozitie].count('1')+int(detectieErori[pozitie + 2])) % 2 == 0:
-                        print("Datele sunt corecte!")
+                        #print("Datele sunt corecte!")
                         lungimiSenzoriInDecimal.append(str(int(str(frameBinar[pozitie]), 2)))
                     else:
                         print("Datele sunt incorecte! Verificati bitii de paritate!")
                 if len(lungimiSenzoriInDecimal) == 5: #verificare daca avem 5 lungimi in lista (daca primim o data gresita nu se adauga la linia 27)
                     lungimiSenzoriInDecimal.append(detectieErori[0])
-                    print(len(lungimiSenzoriInDecimal))
+                    #print(len(lungimiSenzoriInDecimal))
 
                     #fisierCitireDate = open("verif.txt", "a")
                     #fisierCitireDate.write(" ".join(dist))
@@ -40,6 +40,7 @@ def decodificareDate():
         if linieDateFifo=="Send data!\0":
             print ("Data request was send!")
             fifoGUI = open(locatieFifo, "w")
+            print(" ".join(lungimiSenzoriInDecimal) + "\0")
             fifoGUI.write(" ".join(lungimiSenzoriInDecimal) + "\0")
             fifoGUI.close()
 
